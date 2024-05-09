a4d7a66eb0cad914d4655889d375bbf0c553c1a3
James Fishwick <fishwick@gmail.com>
2024-05-09 11:09:28 -0400

    Initial Commit

---
+++ FlowFinder/FlowFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowFinderApp.swift
+//  FlowFinder
+//
+//  Created by James Fishwick on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FlowFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
