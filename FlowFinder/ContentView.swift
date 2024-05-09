f9483f54d7869b287f55b9ff63be6c18ce65ccbe
James Fishwick <fishwick@gmail.com>
2024-05-09 14:52:02 -0400

    Inital commit. Idomatic and elegant use of Measurement and UnitLength if I do say so myself.

--- FlowFinder/ContentView.swift
+++ FlowFinder/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    private let units: [UnitLength] = [.meters, .kilometers, .feet, .yards, .miles]
+    @State private var inputAmount = ""
+    @State private var startUnit = 0
+    @State private var endUnit = 1
+    
+    private var convertedValue: Double {
+            let unitToConvert = Measurement(value: Double(inputAmount) ?? 0, unit: units[startUnit])
+            return unitToConvert.converted(to: units[endUnit]).value
         }
-        .padding()
+    
+    var body: some View {
+        NavigationStack {
+            Form{
+                Section("What are you converting from?") {
+                    
+                    TextField("Enter Amount:", text: $inputAmount)
+                                               .keyboardType(.decimalPad)
+                                        Picker("Starting Unit", selection: $startUnit) {
+                                            ForEach(0..<units.count, id: \.self) {
+                                                Text(self.units[$0].symbol)
+                                            }
+                                        }
+                                        .pickerStyle(SegmentedPickerStyle())
+                }
+                Section("What unit are you converting to?") {
+                    
+                    Picker("End Unit", selection: $endUnit) {
+                                            ForEach(0..<units.count, id: \.self) {
+                                                Text(self.units[$0].symbol)
+                                            }
+                                        }.pickerStyle(.segmented)
+                    Text("\(convertedValue, specifier: "%.4g")")
+                }
+            }
+        }
     }
 }
 
